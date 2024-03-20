db71897c03d70b6770b022fe926e7efee7d3c689
manikandansakthivel4840 <163383528+manikandansakthivel4840@users.noreply.github.com>
2024-03-20 09:51:31 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,7 @@
+function bulboff()
+{
+    document.getElementById("img").src="./light-bulb-on-off-png-115539402943y50vxr5yi.png";
+}
+function bulbon(){
+    document.getElementById("img").src="./light-bulb-on-off-png-11553940319kdxsp3rf0i.png";
+}
